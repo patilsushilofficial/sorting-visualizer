594b0e8c2958bbecc359589f9fd1ca4ae3054f94
Sushil Patil <patilsushilofficial@gmail.com>
2024-07-30 00:56:16 +0530

    Added Tabview in the project

--- App.js
+++ App.js
@@ -1,30 +1,61 @@
 
-import { SafeAreaView, ScrollView, Text, View, StyleSheet } from 'react-native';
+import { SafeAreaView, ScrollView, Text, View, StyleSheet,useWindowDimensions,StatusBar } from 'react-native';
 import BubbleSort from './components/SortingAlgorithms/BubbleSort';
 import SelectionSort from './components/SortingAlgorithms/SelectionSort';
 import InsertionSort from './components/SortingAlgorithms/InsertionSort';
 import MergeSort from './components/SortingAlgorithms/MergeSort';
 import QuickSort from './components/SortingAlgorithms/QuickSort';
+import { TabView, SceneMap,TabBar } from 'react-native-tab-view';
+import { useState } from'react';
+
+
+const renderScene = SceneMap({
+  first: BubbleSort,
+  second: SelectionSort,
+  third:InsertionSort,
+  fourth:MergeSort,
+  fifth:QuickSort,
+});
 
 export default function App() {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'first', title: 'Bubble Sort' },
+    { key: 'second', title: 'Selection Sort' },
+    { key: 'third', title: 'Insertion Sort' },
+    { key: 'fourth', title: 'Merge Sort' },
+    { key: 'fifth', title: 'Quick Sort' },
+    { key: 'fifth', title: 'Quick Sort' },
+  ]);
+
+
   return (
-    <SafeAreaView>
-      <ScrollView contentContainerStyle={styles.container}>
-        <Text style={styles.header}>Sorting Algorithms Visualization</Text>
-        <BubbleSort />
-        <SelectionSort />
-        <InsertionSort />
-        <MergeSort />
-        <QuickSort />
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="teal" barStyle="light-content" />
+     <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+      renderTabBar={props => (
+        <TabBar
+          {...props}
+          scrollEnabled={true}
+          indicatorStyle={{ backgroundColor: 'white' }}
+          style={{ backgroundColor: 'teal' }}
+          labelStyle={{ color: 'white', fontWeight: 'bold' }}
+        />
+      )}
+    />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
-    alignItems: 'center',
+    flex:1
   },
   header: {
     fontSize: 24,
